7df1b88cea55041b26671aa2628924d4f16408ba
gulei.sonacy <gulei.sonacy@bytedance.com>
2020-11-20 18:57:17 +0800

    fix: asd

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,21 @@ module.exports = {
   plugins: [
     {
       resolve: "gatsby-theme-mdx-deck",
-      options: {},
+      options: {
+        cli: true,
+        contentPath: "decks",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-compile-es6-packages",
+      options: {
+        modules: [
+          "mdx-deck",
+          "gatsby-theme-mdx-deck",
+          "@mdx-deck/themes",
+          "@mdx-deck",
+        ],
+      },
     },
   ],
 };
